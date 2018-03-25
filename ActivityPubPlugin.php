cc454c2a378e1a6e155606a3d8a17907ef3fc352
Daniel Supernault <danielsupernault@gmail.com>
2018-03-25 14:16:43 -0600

    Add base plugin and the beginning of the actor method

---
+++ ActivityPubPlugin.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * GNU social - a federating social network
+ *
+ * Plugin that handles ActivityPub
+ *
+ * PHP version 5
+ *
+ * LICENCE: This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Plugin
+ * @package   GNUsocial
+ * @author    Daniel Supernault <danielsupernault@gmail.com>
+ * @copyright 2014 Free Software Foundation http://fsf.org
+ * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
+ * @link      https://www.gnu.org/software/social/
+ */
+
+if (!defined('GNUSOCIAL')) { exit(1); }
+
+class ActivityPubPlugin extends Plugin
+{
+
+    public function onRouterInitialized(URLMapper $m)
+    {
+        $m->connect('api/statuses/user_timeline/:id.ap',
+                    ['action' => 'activitypubactor'],
+                    ['id'     => '[0-9]+']);
+    }
+
+    public function onPluginVersion(array &$versions)
+    {
+        $versions[] = [ 'name' => 'ActivityPub',
+                        'version' => GNUSOCIAL_VERSION,
+                        'author' => 'Daniel Supernault',
+                        'homepage' => 'https://www.gnu.org/software/social/',
+                        'rawdescription' =>
+                        // Todo: Translation
+                        'Adds ActivityPub Support'];
+        return true;
+    }
+}
---
+++ actions/activitypubactor.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * GNU social - a federating social network
+ *
+ * Todo: Description
+ *
+ * PHP version 5
+ *
+ * LICENCE: This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Plugin
+ * @package   GNUsocial
+ * @author    Daniel Supernault <danielsupernault@gmail.com>
+ * @copyright 2015 Free Software Foundaction, Inc.
+ * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
+ * @link      https://gnu.io/social
+ */
+
+if (!defined('GNUSOCIAL')) { exit(1); }
+
+class ActivityPubActorAction extends ManagedAction
+{
+    protected $needLogin = false;
+    protected $canPost   = true;
+
+    protected $user = false;
+
+    protected function handle()
+    {
+        $this->user = User::getByID($this->trimmed('id'));
+
+        $res = [
+          '@context' => ["https://www.w3.org/ns/activitystreams"],
+          'id' => $this->user->uri,
+          'type' => 'Person'
+
+        ];
+
+        header('Content-Type: application/json');
+        echo json_encode($res, JSON_PRETTY_PRINT);
+    }
+}
